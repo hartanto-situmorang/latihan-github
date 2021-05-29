0df8dfb3d88901bede6c58118e9d2ed4eee0d7c9
hartanto-situmorang <85005837+hartanto-situmorang@users.noreply.github.com>
2021-05-29 16:13:16 +0700

    Create My_profil.php

---
+++ My_profil.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title></title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css"/>
+  <link rel="stylesheet" href="../css/styleprofil.css">
+</head>
+<style>
+    body{
+        height: 100vh;
+        background: url(../img/pic.jpg) no-repeat center;
+        background-size: cover;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+</style>
+<body>
+
+<div class="spasi">
+    <div class="profile-card">
+        <div class="card-header">
+        <div class="pic">
+            <img src="../img/foto.jpg" alt="">
+        </div>
+        <div class="name">HARTANTO SITUMORANG</div>
+        <div class="desc">Teknik Informatika</div>
+        <div class="sm">
+            <a href="https://web.facebook.com/?_rdc=1&_rdr" class="fab fa-facebook-f"></a>
+            <a href="https://twitter.com/Hartant73244065" class="fab fa-twitter"></a>
+            <a href="https://api.whatsapp.com/send?phone=6283157682848" class="fab fa-whatsapp"></a>
+            <a href="https://www.instagram.com/alone_levelup/" class="fab fa-instagram"></a>
+        </div>
+            <a href="https://api.whatsapp.com/send?phone=6283157682848" class="contact-btn">Contact Me</a>
+        </div>
+    </div>
+</div>
+    
+<div class="spasi">
+    <div class="profile-card">
+        <div class="card-header2">
+            <div class="name2">PROJECT UJIAN TENGAH SEMESTER</div>
+            <a href="../view/view.php" class="contact-btnback">Back TO Home</a>
+        </div>
+        </div>
+    </div>
+</div>    
+</body>
+</html>
